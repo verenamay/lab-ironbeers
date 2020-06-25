441376f4619cd03c28a724711bf6b6e1b3dbceb5
vmayrock <verena.mayrock@gmail.com>
2020-06-25 13:16:02 +0200

    working on it

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const hbs = require('hbs');
 const path = require('path');
+const handlebars=require('hbs'); // register the handlebars
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
@@ -13,6 +14,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+handlebars.registerPartials(__dirname + '/views/partials');
 
 // ...
 
@@ -21,5 +23,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+  res.render(__dirname + '/views/partials/beers');
+});
+
+app.get('/random-beer', (req, res) => {
+  res.render(__dirname + '/views/partials/randomBeer');
+});
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
